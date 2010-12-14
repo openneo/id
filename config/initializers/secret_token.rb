b246705a82d429d026803ef2b9a11feefa5b2e9a
Matchu <matchu1993@gmail.com>
2010-12-14 08:33:24 -0500

    initial commit: rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenneoIdRails::Application.config.secret_token = 'd4695104c0ca37d4a6790f6a4383888b468148d5cf2a53587b4913fed78149dc78d409e395a91f83c482a945ced8730cf53e5d3819519a4974859ff8964271dc'
