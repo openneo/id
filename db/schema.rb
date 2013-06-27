d6df4715828a8489ae2cb42f12cb53feb64465d4
Matchu <matchu1993@gmail.com>
2013-06-26 21:03:05 -0700

    add reset_password_sent_at to users for devise 2.x support

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,24 +11,25 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110105011057) do
+ActiveRecord::Schema.define(:version => 20130627040226) do
 
   create_table "users", :force => true do |t|
-    t.string   "name",                 :limit => 20,                :null => false
-    t.string   "encrypted_password",   :limit => 64,                :null => false
-    t.string   "email",                :limit => 50,                :null => false
-    t.string   "password_salt",        :limit => 32,                :null => false
+    t.string   "name",                   :limit => 20,                :null => false
+    t.string   "encrypted_password",     :limit => 64,                :null => false
+    t.string   "email",                  :limit => 50,                :null => false
+    t.string   "password_salt",          :limit => 32,                :null => false
     t.string   "reset_password_token"
-    t.integer  "sign_in_count",                      :default => 0
+    t.integer  "sign_in_count",                        :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",                    :default => 0
+    t.integer  "failed_attempts",                      :default => 0
     t.string   "unlock_token"
     t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "reset_password_sent_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
